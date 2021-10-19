6c494d651e7f379a89d6e99098e815edf18a9fd4
Jun <mambo237@yahoo.com>
2021-10-18 23:04:50 -0400

    Lab4 demo code corrected upload

--- lab-4.py
+++ lab-4.py
@@ -7,12 +7,13 @@ def syncTables(event, context):
 
 # Scan returns paginated results, so only partial data will be copied
 def sync_ddb_table(source_ddb, destination_ddb):
-    response = source_ddb.scan(
-        TableName="<FMI1>"
-    )
 
-    for item in response['Items']:
-        destination_ddb.put_item(
-            TableName="<FMI2>",
-            Item=item
-        )
+    response = source_ddb.scan(TableName="<FMI1>")
+
+    paginator = response.get_paginator('list_items')
+
+    page_iterator = paginator.paginate()
+
+    for page in page_iterator:
+        for item in page['Items']:
+            destination_ddb.put_item(TableName="<FMI2>", Item=item)
